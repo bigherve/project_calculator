8ebbff0a5e30f8f8061f5d50b4f3085679786183
bigherve <hervekunda@gmail.com>
2023-02-23 20:32:11 +0100

    Add all needed html and basic operate function

---
+++ script.js
@@ -0,0 +1,55 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function power(num1, num2) {
+    return num1 ** num2;
+}
+
+/*function multiply(...nums) {
+    return nums.reduce((acumilator, item) => acumilator * item, 1);
+}
+
+function division(...nums) {
+    return nums.reduce((acumilator, item) => acumilator / item);
+}*/
+
+function operate1(opp, num1, num2) {
+    if (opp === '+') {
+        return add(num1, num2);
+    } else if (opp === '-') {
+        return subtract(num1, num2);
+    } else if (opp === '*') {
+        return multiply(num1, num2);
+    } else if (opp === '/') {
+        return divide(num1, num2);
+    } else if (opp === '**') {
+        return power(num1, num2);
+    }
+}
+
+function operate2(operator, number1, number2) {
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+        '**': (a, b) => a ** b,
+    };
+
+    if (operator in operators) {
+        return operators[operator](number1, number2);
+    } else return 'ERROR!';
+}
